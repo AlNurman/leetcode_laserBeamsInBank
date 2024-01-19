7d7cbb955d807afe503ad349da10a823d4a01f01
Almaz Nurmanov <almazbrr@gmail.com>
2024-01-18 22:10:54 -0500

    done

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,22 @@
+fun numberOfBeams(bank: Array<String>): Int {
+	var res=0
+	var mlt=0
+	for(i in 0 until bank.size){
+		val str1 = bank[i].replace("0", "")
+		if (str1.length>0) {
+			if (mlt == 0) {
+				mlt = str1.length
+			} else {
+				mlt*=str1.length
+				res+=mlt
+				mlt=str1.length
+			}
+		}
+	}
+	return res
+}
 fun main() {
-    println("Hello World!")
+	println("Hello World!")
+	val bank = arrayOf("011001","000000","010100","001000")
+	println(numberOfBeams(bank))
 }
